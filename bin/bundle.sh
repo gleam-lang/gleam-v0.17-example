341d72eaea647e1cd1d67303ac69c4a7bc80399a
Louis Pilfold <louis@lpil.uk>
2021-09-19 20:05:05 +0100

    v0.17 example

--- bin/bundle.sh
+++ bin/bundle.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 set -eu
 
-./node_modules/.bin/esbuild target/lib/worker-gleam-template/index.js --bundle --outfile=target/dist/index.js
+./node_modules/.bin/esbuild target/lib/gleam-v0.17-example/index.js --bundle --outfile=target/dist/index.js
